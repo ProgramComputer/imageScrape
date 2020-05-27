ce4381b34ff20031bb4cfe122e8e76dade320d46
Paul <22284856+ProgramComputer@users.noreply.github.com>
2020-05-27 16:25:03 -0500

    Update scrapeanddownload.py

--- scrapeanddownload.py
+++ scrapeanddownload.py
@@ -1,9 +1,29 @@
 
-#credit to https://code.tutsplus.com/tutorials/compressing-and-extracting-files-in-python--cms-26816
+#credit to https://code.tutsplus.com/tutorials/compressing-and-extracting-files-in-python--cms-26816 and https://rubikscode.net/2019/12/02/scraping-images-with-python/
+from bs4 import BeautifulSoup
+import zlib
+import requests
+import urllib.request
+import shutil
+
 import os
 import zipfile
  
-fantasy_zip = zipfile.ZipFile('C:\\Stories\\Fantasy\\archive.zip', 'w')
+url = "https://rubikscode.net/"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+aas = soup.find_all("a", class_='entry-featured-image-url')
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ fantasy_zip = zipfile.ZipFile('C:\\Stories\\Fantasy\\archive.zip', 'w')
  
 for folder, subfolders, files in os.walk('C:\\Stories\\Fantasy'):
  
